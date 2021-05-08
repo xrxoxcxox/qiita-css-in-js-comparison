5370f045a63b29fa5e8f61cd4052fa091303f925
xrxoxcxox <watanuki@ju-zen.com>
2021-05-08 23:20:09 +0900

    Add global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
+import { FC } from 'react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
